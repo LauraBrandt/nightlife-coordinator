c7dd9245bb61f6b2ee4d087571373f9fe9fc603a
Brandt <laurambrandt@gmail.com>
2018-02-26 17:01:46 -0300

    if user not auth, instead of showing popover with attendees, shows a dialog instructing the user to log in

--- src/Bar.js
+++ src/Bar.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import Popover from 'material-ui/Popover';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import Checkbox from 'material-ui/Checkbox';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
@@ -12,11 +14,16 @@ class Bar extends Component {
 
     this.state = {
       popoverOpen: false,
-      popoverAnchorEl: {}
+      popoverAnchorEl: {},
+      dialogOpen: false
     }
 
     this.handleShowPopover = this.handleShowPopover.bind(this);
     this.handleRequestClosePopover = this.handleRequestClosePopover.bind(this);
+    this.handleDialogOpen = this.handleDialogOpen.bind(this);
+    this.handleDialogClose = this.handleDialogClose.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
   }
 
   handleShowPopover(e) {
@@ -29,13 +36,45 @@ class Bar extends Component {
   }
 
   handleRequestClosePopover = () => {
-    this.setState({
-      popoverOpen: false,
-    });
+    this.setState({ popoverOpen: false });
   };
 
+  handleDialogOpen = () => {
+    this.setState({ dialogOpen: true });
+  };
+
+  handleDialogClose = () => {
+    this.setState({ dialogOpen: false });
+  };
+
+  handleLogin = () => {
+    window.location.href = '/login';
+  }
+
+  handleSignup = () => {
+    window.location.href = '/signup';
+  }
+
   render() {
-    const { bar, user, isAuth } = this.props
+    const { bar, user, isAuth } = this.props;
+
+    const actions = [
+      <FlatButton
+        label="Log In"
+        primary={true}
+        onClick={this.handleLogin}
+      />,
+      <FlatButton
+        label="Sign Up"
+        secondary={true}
+        onClick={this.handleSignup}
+      />,
+      <FlatButton
+        label="Cancel"
+        onClick={this.handleDialogClose}
+      />
+    ];
+
     return (
       <Paper zDepth={2} className="bar__paper">
         <img src={bar.image_url} alt="" className="bar__img"/>
@@ -44,7 +83,7 @@ class Bar extends Component {
           <RaisedButton 
             label={`${bar.attendees.length} Going`}
             primary={true}
-            onClick={this.handleShowPopover}
+            onClick={isAuth ? this.handleShowPopover : this.handleDialogOpen}
           />
           <Popover
             open={this.state.popoverOpen && bar.attendees.length > 0}
@@ -57,6 +96,14 @@ class Bar extends Component {
               {bar.attendees.map(attendee => <MenuItem primaryText={attendee.displayName} key={attendee._id}/>)}
             </Menu>
           </Popover>
+          <Dialog
+            title="Please Log In"
+            actions={actions}
+            open={this.state.dialogOpen}
+            onRequestClose={this.handleDialogClose}
+          >
+            You must be logged in to see who's going.
+          </Dialog>
           <Checkbox
             label="I'm in!"
             name={bar.id}
