ac70dd723a1d4d7a5cebf589f58ff7da8c8f6415
Brandt <laurambrandt@gmail.com>
2018-03-04 20:31:14 -0300

    minor style changes to header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
-import {grey400} from 'material-ui/styles/colors';
+import {grey300} from 'material-ui/styles/colors';
 
 const Header = (props) => {
   return (
@@ -34,8 +34,8 @@ const Header = (props) => {
       </nav>
       {props.textShowing && !props.loading && <div className="header__text">
         <h1 className="header__h1">Nightlife Coordinator</h1>
-        <p style={{color: grey400}}><i className="material-icons">local_bar</i>See what bars people are going to.</p>
-        <p style={{color: grey400}}><i className="material-icons">person_pin_circle</i>Then RSVP to your favorite.</p>
+        <p style={{color: grey300}}><i className="material-icons">local_bar</i>See what bars people are going to.</p>
+        <p style={{color: grey300}}><i className="material-icons">person_pin_circle</i>Then RSVP to your favorite.</p>
       </div>}
     </header>
   );
