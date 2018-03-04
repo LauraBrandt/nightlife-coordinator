8948ab8ef66a5ad9c1ceae7a3a1479b0bdbf8c52
Brandt <laurambrandt@gmail.com>
2018-03-04 20:32:07 -0300

    tidying

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {teal400, indigo400, grey100, grey50, fullWhite} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-// import './index.css';
 import App from './App';
 import Login from './Login';
 import Signup from './Signup';
@@ -27,7 +26,7 @@ const muiTheme = getMuiTheme({
 
 ReactDOM.render(
   <MuiThemeProvider muiTheme={muiTheme}>
-     <BrowserRouter>
+    <BrowserRouter>
       <div>
         <Switch>
           <Route exact path='/login' render={() => {
@@ -47,7 +46,7 @@ ReactDOM.render(
         </Switch>
         <Footer />
       </div>
-     </BrowserRouter>
-   </MuiThemeProvider>, 
+    </BrowserRouter>
+  </MuiThemeProvider>, 
   document.getElementById('root'));
 registerServiceWorker();
